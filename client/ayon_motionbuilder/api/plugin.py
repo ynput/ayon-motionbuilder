9ef33fc04b26893521f386165897cd23be1125d3
Kayla Man <moonyuet2008@hotmail.com>
2024-06-12 22:57:26 +0800

    add creator plugins

--- client/ayon_motionbuilder/api/plugin.py
+++ client/ayon_motionbuilder/api/plugin.py
@@ -1,10 +1,17 @@
 """Motion Builder specific AYON/Pyblish plugin definitions."""
+from abc import ABCMeta
+import six
+from ayon_core.lib import BoolDef
 from ayon_core.pipeline import (
+    CreatedInstance,
+    Creator,
+    CreatorError,
     AYON_INSTANCE_ID,
     AVALON_INSTANCE_ID,
 )
 
-from .lib import lsattr
+from pyfbsdk import FBSet, FBSystem
+from .lib import lsattr, imprint, read, get_node_by_name
 
 
 class MotionBuilderCreatorBase(object):
@@ -31,4 +38,85 @@ class MotionBuilderCreatorBase(object):
                             "mbuilder_cached_instances"][creator_id].append(i.name)
         return shared_data
 
+    def create_node(self, product_name):
+        container_node = get_node_by_name(product_name)
+        if not container_node:
+            container_node = FBSet(product_name)
+            return container_node.Name
 
+@six.add_metaclass(ABCMeta)
+class MotionBuilderCreator(Creator, MotionBuilderCreatorBase):
+
+    def create(self, product_name, instance_data, pre_create_data):
+        if get_node_by_name(product_name):
+            raise CreatorError(f"'{product_name}' is already created..")
+        creator_attributes = instance_data.setdefault(
+            "creator_attributes", dict())
+        for key in [
+            "SaveSelectedModelsOnly",
+            "KeepTransformHierarchy",
+        ]:
+            if key in pre_create_data:
+                creator_attributes[key] = pre_create_data[key]
+        instance_node = self.create_node(product_name)
+        instance_data["instance_node"] = instance_node
+        if pre_create_data.get("SaveSelectedModelsOnly"):
+            instance_data["selected_nodes"] = get_selection()
+            node = get_node_by_name(instance_node)
+            for sel in get_selection():
+                node.ConnectSrc(sel)
+
+        instance = CreatedInstance(
+            self.product_type,
+            product_name,
+            instance_data,
+            self
+        )
+        self._add_instance_to_context(instance)
+        imprint(instance_node, instance.data_to_store())
+        return instance
+
+    def collect_instances(self):
+        self.cache_instance_data(self.collection_shared_data)
+        cached_instances = self.collection_shared_data["mbuilder_cached_instances"]
+        for instance in cached_instances.get(self.identifier, []):
+            created_instance = CreatedInstance.from_existing(
+                read(get_node_by_name(instance)), self
+            )
+            self._add_instance_to_context(created_instance)
+
+    def update_instances(self, update_list):
+        for created_inst, _ in update_list:
+            instance_node = created_inst.get("instance_node")
+            imprint(
+                instance_node,
+                created_inst.data_to_store()
+            )
+
+    def remove_instances(self, instances):
+        """Remove specified instance from the scene.
+
+        This is only removing `id` parameter so instance is no longer
+        instance, because it might contain valuable data for artist.
+
+        """
+        for instance in instances:
+            instance_node = get_node_by_name(instance.data.get("instance_node"))
+            if instance_node:
+               instance_node.FBDelete()
+            self._remove_instance_from_context(instance)
+
+
+    def get_pre_create_attr_defs(self):
+        return [
+            BoolDef("SaveSelectedModelsOnly",
+                    label="Save Selected Models Only"),
+            BoolDef("KeepTransformHierarchy",
+                    label="Keep Transform Hierarchy"),
+        ]
+
+def get_selection():
+    return [
+        component.Name for component in FBSystem().Scene.Components
+        if component.Selected == True and component.ClassName == "FBModel"
+    ]
