df82069546c8ffe78864bbb0d94271b6cb5ea435
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-07-28 15:32:35 +0200

    use workfiles api to bump version workfile

--- client/ayon_motionbuilder/plugins/publish/increment_workfile_version.py
+++ client/ayon_motionbuilder/plugins/publish/increment_workfile_version.py
@@ -1,4 +1,7 @@
+import os
+
 import pyblish.api
+
 from ayon_core.lib import version_up
 from ayon_core.pipeline import registered_host
 
@@ -14,5 +17,31 @@ class IncrementWorkfileVersion(pyblish.api.ContextPlugin):
     def process(self, context):
         host = registered_host()
         path = context.data["currentFile"]
-        self.log.info(f"Increment and save workfile: {path}")
-        host.save_workfile(version_up(path))
+        try:
+            from ayon_core.pipeline.workfile import save_next_version
+            from ayon_core.host.interfaces import SaveWorkfileOptionalData
+
+            current_filename = os.path.basename(path)
+            save_next_version(
+                description=(
+                    f"Incremented by publishing from {current_filename}"
+                ),
+                # Optimize the save by reducing needed queries for context
+                prepared_data=SaveWorkfileOptionalData(
+                    project_entity=context.data["projectEntity"],
+                    project_settings=context.data["project_settings"],
+                    anatomy=context.data["anatomy"],
+                )
+            )
+            new_path = host.get_current_workfile()
+
+        except ImportError:
+            # Backwards compatibility before ayon-core 1.5.0
+            self.log.debug(
+                "Using legacy `version_up`. Update AYON core addon to "
+                "use newer `save_next_version` function."
+            )
+            new_path = version_up(path)
+            host.save_workfile(new_path)
+
+        self.log.info(f"Increment and save workfile: {new_path}")
