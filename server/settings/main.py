c47afc178a2f4c32ecf9f06d5bb14dc2a83c8e48
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-03-05 13:13:14 +0100

    added settings for create plugins

--- server/settings/main.py
+++ server/settings/main.py
@@ -1,8 +1,14 @@
 from ayon_server.settings import BaseSettingsModel, SettingsField
 
+from .create import CreatePluginsModel
+
 DEFAULT_VALUES = {}
 
 
 class MotionBuilderSettings(BaseSettingsModel):
     pass
+    create: CreatePluginsModel = SettingsField(
+        title="Create plugins",
+        default_factory=CreatePluginsModel,
+    )
 
