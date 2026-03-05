c8e3bf3e251aeec543e1ea1b8fe475ff1c85a31b
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-03-05 13:13:02 +0100

    move settings to subfolder

--- server/settings/main.py
+++ server/settings/main.py
@@ -1,7 +1,8 @@
-from ayon_server.settings import BaseSettingsModel
+from ayon_server.settings import BaseSettingsModel, SettingsField
 
 DEFAULT_VALUES = {}
 
 
 class MotionBuilderSettings(BaseSettingsModel):
     pass
+
